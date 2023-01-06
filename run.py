1cb08794ef85c17ebee6741fa139172a93459fa8
georgiagrayland <georgiamayeva@live.co.uk>
2023-01-06 16:54:31 +0000

    create initial part of function to add expenses to a list

--- run.py
+++ run.py
@@ -9,4 +9,16 @@ from datetime import date
 ITEMS_OR_RESOURCES = []
 PRICES = []
 DATES = []
-EXPENSE_CATEGORY = []
+EXPENSE_GROUPS = []
+
+#Function to add expenses to lists and structure input data 
+
+def add_expense(item_or_resource, price, date, expense_group):
+    """
+    Adds all aspects of each expense to a list. 
+    Stores the data input by the for each part of an individual expense. 
+    """
+    ITEMS_OR_RESOURCES.append(item_or_resource)
+    PRICES.append(price)
+    DATES.append(date)
+    EXPENSE_GROUPS.append(expense_group)
