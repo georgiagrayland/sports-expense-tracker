55b8b238b7a241250668861ebd10ee3b4a35eb44
georgiagrayland <georgiamayeva@live.co.uk>
2023-01-06 16:37:06 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+#Import libraries
+import numpy as np
+import pandas as pd
+from datetime import date
+
+#Create empty lists to organise data
+ITEMS_OR_RESOURCES = []
+PRICES = []
+DATES = []
+EXPENSE_CATEGORY = []
