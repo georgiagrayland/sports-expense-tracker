7b142177886104b1eacae05bede12d6e8605b5e8
georgiagrayland <georgiamayeva@live.co.uk>
2023-01-06 17:13:49 +0000

    create initial options output display for the user

--- run.py
+++ run.py
@@ -22,3 +22,17 @@ def add_expense(item_or_resource, price, date, expense_group):
     PRICES.append(price)
     DATES.append(date)
     EXPENSE_GROUPS.append(expense_group)
+
+
+#Output and processing program
+choice = -1 #This will be the number input by the user 
+while choice != 0:
+    #Create and display input choice list to users
+    print('Welcome to Athleticoin, \n Manage your sports expenses here! \n')
+    print('1. Create Equipment Expense')
+    print('2. Create Coaching Expense')
+    print('3. Create Transport Expense')
+    print('4. Create Clothing or Footwear Expense')
+    print('5. Allocate to savings')
+    print('6. Save Expenses and Show All')
+    print('0. Exit and Clear')
