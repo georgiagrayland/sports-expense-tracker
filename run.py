b1d7633f5f49f928358fa8018935d7fe6caa5fb5
georgiagrayland <georgiamayeva@live.co.uk>
2023-01-12 17:09:01 +0000

    edit savings functions to validate incorrect user input

--- run.py
+++ run.py
@@ -99,11 +99,12 @@ def input_savings_number():
             print(
                 'Invalid input. Please enter an amount to add to savings'
                 )
+            input_savings_number()
         else:
             formatted_price = round(float(price), 2)
             add_expense(
                 item_or_resource, formatted_price, date, expense_group)
-            break
+        break
 
 
 def input_savings_reason():
