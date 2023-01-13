f667a0608e8a4bfa386370d4367db71dd208e172
georgiagrayland <georgiamayeva@live.co.uk>
2023-01-13 11:43:51 +0000

    create functions for adding expense name and prices

--- run.py
+++ run.py
@@ -87,6 +87,40 @@ def input_choices():
     print('0. Exit and Clear\n')
 
 
+def input_expense_price():
+    """
+    User inputs price of expense after name given
+    Only accepts numbers as input
+    """
+    price = input('Enter the price of this expense: \n')
+    if not re.search(r'^[-+]?[0-9]*\.?[0-9]+$', price):
+        print(
+            'Invalid input. Please enter a number')
+        input_expense_price()
+    else:
+        formatted_price = round(float(price), 2)
+        add_expense(
+            item_or_resource, formatted_price, date, expense_group)
+        input_choices()
+
+
+def input_expense_name():
+    """
+    User inputs name of expense under chosen group
+    Requires input to continue to price input
+    """
+    while True:
+        global item_or_resource
+        item_or_resource = input(
+                f'Enter the name of this {expense_group} expense\n')
+        if item_or_resource == "":
+            print('Please assign an expense name!')
+            input_expense_name()
+        else:
+            input_expense_price()
+            break
+
+
 def input_savings_number():
     """
     Prompts user to enter value of savings
@@ -124,49 +158,55 @@ def input_savings_reason():
         input_savings_number()
   
 
-# MAKE THIS A FUNCTION
+# MAKE THIS A FUNCTION??
 # Output and processing program
-while True:  # choice != 0:  # Put this while loop in a function??
+# while True:  # choice != 0:  # Put this while loop in a function??
     # Create and display input choice list to users
     # show_intro()
-    input_choices()
-    choice = input('Please choose an action: \n')
-    print()
+input_choices()
+choice = input('Please choose an action: \n')
+print()
 
-    # Check input choice from the user - make this a function to call in main?
+# Check input choice from the user - make this a function to call in main?
+while True:
     if choice == '0':
         print('You are exiting the program')
         break
     elif choice == '1':
-        print('Add Equipment Cost')
+        # print('Add Equipment Cost')
         expense_group = 'EQUIPMENT'
+        input_expense_name()
     elif choice == '2':
         print('Add Coaching Cost')
         expense_group = 'COACHING'
+        input_expense_name()
     elif choice == '3': 
         print('Add Transport Cost')
         expense_group = 'TRANSPORT'
+        input_expense_name()
     elif choice == '4':
         print('Add Clothing or Footwear Cost')
         expense_group = 'CLOTHING'
+        input_expense_name()
     elif choice == '5':
         print('Allocate to Savings Fund')
         input_savings_reason()
         expense_group = 'SAVINGS'
     elif choice == '6':
-        # Show Expenses in Data Frame 
+    # Show Expenses in Data Frame 
         show_report()
     elif not re.search(r'^[-+]?[0-6]+$', choice):
         print('Invalid input, please enter a value from the options!')
-        # input_choices()  # CHANGE THIS TO SHOW OPTIONS WITH NO INTRO??
+    # input_choices()  # CHANGE THIS TO SHOW OPTIONS WITH NO INTRO??
     else:
         print('You chose an invalid number.')
         print('Please choose a number option between 0 and 6\n')
-        # input_choices()  # CHANGE THIS TO SHOW OPTIONS WITH NO INTRO??
+    # input_choices()  # CHANGE THIS TO SHOW OPTIONS WITH NO INTRO??
 
 
 # Create inputs for user to add price and expense name
-
+    
+    """
     if choice == '1' or choice == '2' or choice == '3' or choice == '4':
         item_or_resource = input(
             f'Enter the name of this {expense_group} expense\n')
@@ -183,7 +223,7 @@ while True:  # choice != 0:  # Put this while loop in a function??
                 item_or_resource, formatted_price, date, expense_group)
 
 
-"""
+
     if choice == '5':
         while True:
             item_or_resource = input(
@@ -204,12 +244,3 @@ while True:  # choice != 0:  # Put this while loop in a function??
                     item_or_resource, formatted_price, date, expense_group)
                 break
 """
-
-
-
-
-
-
-
-
-
